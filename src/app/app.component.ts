a3687e4b232b28c3ff8665489096fc55ff86c44a
NajhelyBS <s5anbandasanchez@gmail.com>
2022-12-29 11:07:42 -0500

    Iniciando denuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'actividad-7';
+
+  numeroAleatorio(){
+    var dado;
+    lados: 6;
+
+    var randomNumber = Math.floor(Math.random() * this.lados) + 1;
+    return randomNumber;
+    
+  }
+
 }
