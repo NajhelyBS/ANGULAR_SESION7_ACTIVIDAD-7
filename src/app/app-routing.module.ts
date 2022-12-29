10b4181bb9c2cd2028f3f29043822188e8fc79fa
NajhelyBS <s5anbandasanchez@gmail.com>
2022-12-29 10:52:41 -0500

    Se actualizó el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DadoComponent } from './elementos/dado/dado.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"", component:DadoComponent
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/elementos/dado/dado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+
+  number:Number = 0;
+
+}
